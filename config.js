9904653463a9c5637b212add3201cda08e210315
fave-eng <fhendytheking@gmail.com>
2026-07-08 14:56:52 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-Unsupported Media Type
+window.APP_CONFIG = {
+  BOT_TOKEN: "8604414786:AAEcyyQxHiYUi80PDZt7ZkQjomLfsm_VjKM",
+  CHAT_ID: "-1003908460669",
+  SB_URL: "https://zqzgarvmpqqqaobeicpc.supabase.co",
+  SB_KEY: "sb_publishable_tLdpYQZSbjxOZGQpee_jMQ_lfOBYZxC",
+  STUDENT: "polina"
+};
